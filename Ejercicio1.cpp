c9c9a8eeab549cc899440e8d27ce966ebc2ea1f6
benjaminsuclle <msuclles@unsa.edu.pe>
2022-10-07 13:26:37 -0500

    update

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -4,79 +4,61 @@
 #include <stdlib.h>
 using namespace std;
 
-int obtenerNumeroPositivo ();
-void rellenarArreglo (int *arrintdenumeros, int inttamano);
-void imprimirArreglo (int *arrintdenumeros, int inttamano);
-void impresordeImparesyRutas (int *arrintdenumeros, int inttamano);
+int obtenerNumeroPositivo();
+void rellenarArreglo(int *arrintdenumeros, int inttamano);
+void imprimirArreglo(int *arrintdenumeros, int inttamano);
+void impresordeImparesyRutas(int *arrintdenumeros, int inttamano);
 
-int
-main ()
-{
+int main() {
 
   int intcantdenum;
 
-
   cout << "B?Cuantos nC:meros tendra tu array?: ";
-  intcantdenum = obtenerNumeroPositivo ();
+  intcantdenum = obtenerNumeroPositivo();
   int arrintdenumeros[intcantdenum];
-  rellenarArreglo (arrintdenumeros, intcantdenum);
+  rellenarArreglo(arrintdenumeros, intcantdenum);
   cout << "Tu array es:  ";
-  imprimirArreglo (arrintdenumeros, intcantdenum);
+  imprimirArreglo(arrintdenumeros, intcantdenum);
   cout << "Los nC:meros impares son:  ";
-  impresordeImparesyRutas (arrintdenumeros, intcantdenum);
+  impresordeImparesyRutas(arrintdenumeros, intcantdenum);
   return 0;
 }
 
-int
-obtenerNumeroPositivo ()
-{
+int obtenerNumeroPositivo() {
   int intcantdenum;
-  do
-    {
-      cin >> intcantdenum;	// Asignamos un valor a  la variable cantidad de numeros
-      cout << "has introducido " << intcantdenum << "\n";
-    }
-  while (intcantdenum < 0);
+  do {
+    cin >>
+        intcantdenum; // Asignamos un valor a  la variable cantidad de numeros
+    cout << "has introducido " << intcantdenum << "\n";
+  } while (intcantdenum < 0);
 
   return intcantdenum;
 }
 
-void
-rellenarArreglo (int *arrintdenumeros, int inttamano)
-{
+void rellenarArreglo(int *arrintdenumeros, int inttamano) {
   int numerotemporal;
-  for (int i = 0; i < inttamano; i++)
-    {
-      cout << "Ingresa un numero:";
-      cin >> numerotemporal;
-      arrintdenumeros[i] = numerotemporal;
-    }
+  for (int i = 0; i < inttamano; i++) {
+    cout << "Ingresa un numero:";
+    cin >> numerotemporal;
+    arrintdenumeros[i] = numerotemporal;
+  }
 }
 
-void
-imprimirArreglo (int *arrintdenumeros, int inttamano)
-{
+void imprimirArreglo(int *arrintdenumeros, int inttamano) {
   cout << "[";
-  for (int i = 0; i < inttamano - 1; i++)
-    {
-      cout << arrintdenumeros[i] << ", ";
-    }
+  for (int i = 0; i < inttamano - 1; i++) {
+    cout << arrintdenumeros[i] << ", ";
+  }
   cout << arrintdenumeros[inttamano - 1] << "]\n";
 }
 
-void
-impresordeImparesyRutas (int *arrintdenumeros, int inttamano)
-{
+void impresordeImparesyRutas(int *arrintdenumeros, int inttamano) {
   cout << "[";
-  for (int i = 0; i < inttamano - 1; i++)
-    {
-      if (arrintdenumeros[i] % 2 != 0)
-	{
-	  cout << arrintdenumeros[i] << ", ";
-	}
+  for (int i = 0; i < inttamano - 1; i++) {
+    int intempo = arrintdenumeros[i];
+    if (inttempo % 2 != 0) {
+      cout << inttempo << " : " << intempo* << ", ";
     }
+  }
   cout << arrintdenumeros[inttamano - 1] << "]\n";
-
-
 }
-
